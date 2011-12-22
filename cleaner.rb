425ebbd66c79ba0bfccf801b706266b3463fd06d
andrea longhi <andrea@spaghetticode.it>
2011-12-22 14:48:31 +0100

    join with newline ISO space

--- cleaner.rb
+++ cleaner.rb
@@ -1,11 +1,13 @@
 class Cleaner
+  NEW_LINE = "\n" # use windows newlines
+
   def initialize(codes)
     @codes = codes.map {|code| Code.new(code)}
   end
-  
+
   def remove_control_codes
     @codes.map do |code|
       code.clean
-    end.join(' ')
+    end.join(NEW_LINE)
   end
 end
--- spec/cleaner_spec.rb
+++ spec/cleaner_spec.rb
@@ -1,9 +1,9 @@
 describe Cleaner do
   describe '#remove_control_codes' do
     let(:cleaner) { Cleaner.new(['A-01-C-12-123', 'C-12-L-1-4', 'B-10-K-666']) }
-    
+
     it 'should return a string with codes with control code removed' do
-      cleaner.remove_control_codes.should == 'A-01-C-12 C-12-L-1 B-10-K-666'
+      cleaner.remove_control_codes.should == "A-01-C-12\nC-12-L-1\nB-10-K-666"
     end
   end
 end
